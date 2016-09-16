50fa56fd3d2de2d042548eba1eda69615657dfec
matt2929 <matthewstafford29@gmail.com>
2016-09-16 18:31:56 -0400

    Built Some Structure

---
+++ app/src/main/java/com/example/matthew/scheduleme/Username.java
@@ -0,0 +1,13 @@
+package com.example.matthew.scheduleme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Username extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_username);
+    }
+}
