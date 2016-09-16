d30f66910c6d500b288eebb3782ee09540e72faa
matt2929 <matthewstafford29@gmail.com>
2016-09-16 19:34:36 -0400

    Built Some Structure

--- app/src/main/java/com/example/matthew/scheduleme/Schedule.java
+++ app/src/main/java/com/example/matthew/scheduleme/Schedule.java
@@ -1,13 +1,21 @@
 package com.example.matthew.scheduleme;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
 
 public class Schedule extends AppCompatActivity {
-
+CalendarView cV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        cV =(CalendarView) findViewById(R.id.calendarView);
+        cV.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                
+            }
+        });
     }
 }
