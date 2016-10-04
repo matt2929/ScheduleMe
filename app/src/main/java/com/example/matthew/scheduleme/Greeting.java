3d8ad240d92365a5e63723b783273d8b6d8e82f8
matt2929 <matthewstafford29@gmail.com>
2016-10-04 15:54:42 -0400

    added rest API

---
+++ app/src/main/java/com/example/matthew/scheduleme/Greeting.java
@@ -0,0 +1,18 @@
+package com.example.matthew.scheduleme;
+
+/**
+ * Created by Matthew on 9/30/2016.
+ */
+
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
