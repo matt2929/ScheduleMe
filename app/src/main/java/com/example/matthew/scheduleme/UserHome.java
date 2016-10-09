f2c4157c12c993b4bddc738d5cad2738bf8cf558
matt2929 <matthewstafford29@gmail.com>
2016-10-09 12:20:49 -0400

    removed annoying pink mail button

--- app/src/main/java/com/example/matthew/scheduleme/UserHome.java
+++ app/src/main/java/com/example/matthew/scheduleme/UserHome.java
@@ -1,18 +1,34 @@
 package com.example.matthew.scheduleme;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class UserHome extends AppCompatActivity {
-Button goToCalender;
+    Button goToCalender;
+    Button testRest;
+    String result = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userhome);
-        goToCalender =(Button) findViewById(R.id.userhomeviewschedule);
+        goToCalender = (Button) findViewById(R.id.userhomeviewschedule);
         goToCalender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +38,56 @@ Button goToCalender;
 
             }
         });
+        testRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new HttpRequestTask().execute();
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new HttpRequestTask().execute();
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "https://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                // Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+
+//                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+            String url = "http://warmachine.cse.buffalo.edu:8082/listUsers";
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+            result = restTemplate.getForObject(url, String.class, "Android");
+
+
+            Log.e("Value", "{" + result + "}");
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+
+            //greetingContentText.setText(result);
+        }
+
+    }
+
+
 }
